629e1499c02e5babbeb408a8a8f28cab7e7660a5
Kmc <kmcgurty1@gmail.com>
2021-05-22 11:25:49 -0400

    kill process, split loader and tdmp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const log = require('fancy-log');
 const fs = require('fs');
+const { execSync } = require("child_process");
 
 let fromPath = "C:/Users/Kmc/source/repos/TDMP";
 let toPath = "D:/Steam Games/steamapps/common/Teardown";
@@ -13,19 +14,36 @@ let loader = fromPath + "/TDMPLoader/build/debug/**/*";
 gulp.task('watch', function() {
     gulp.watch('gulpfile.js', exit);
 
-    gulp.watch([tdmp , loader], gulp.series('copy-files'));
+    gulp.watch([tdmp], gulp.series('copy-tdmp'));
+    gulp.watch([loader], gulp.series('copy-loader'));
 });
 
-gulp.task('copy-files', function(e) {
-    gulp.src(tdmp)
-        .pipe(gulp.dest(toPath));
+gulp.task('copy-tdmp', async function(e) {
+    console.log("Killing process...");
+    kill("teardown.exe");
 
+    console.log("Copying TDMP...");
+    return gulp.src(tdmp)
+        .pipe(gulp.dest(toPath));
+});
+
+gulp.task('copy-loader', function(e) {
+    console.log("Copying loader...");
     return gulp.src(loader)
         .pipe(gulp.dest(toPath));
 });
+
+function kill(name){
+    try{
+        let output = execSync(`taskkill /IM "${name}" /F`);
+        console.log(output.toString());
+    } catch(e) {
+        //console.log(e.stderr.toString());
+    }
+}
 
 async function exit() {
     process.exit();
 }
 
-gulp.task('default', gulp.series('copy-files', 'watch'));
+gulp.task('default', gulp.series('copy-tdmp', 'copy-loader', 'watch'));
